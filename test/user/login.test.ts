6da70ece920e56447a9f7c3e1d74323411ff8060
Chiyoku <chiyokuoni@gmail.com>
2021-10-07 21:59:33 -0300

    chore: added boilerplate test for user

---
+++ test/user/login.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest'
+import app from '../../src/server'
+
+test('GET /login', () => {
+  it('Responds with 200', (done) => {
+    request(app).post('/login').expect(200, done)
+  })
+})
